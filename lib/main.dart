b49067e897968d73943c748a8c1c74b5d06877cb
Shahram <shahramdeveloper11@gmail.com>
2024-10-02 18:38:57 +0330

    getApplication widget add to main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,32 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return getApplication();
+  }
+}
+
+Widget getApplication() {
+  return MaterialApp(
+    home: Scaffold(
+      body: SafeArea(
+        child: DecoratedBox(
+          position: DecorationPosition.background,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.fitWidth,
+              image: AssetImage('images/drum.png'),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
